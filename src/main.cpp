8164dcd40c556b7c2ddd5a8e0d23349813f3b4f9
@ITN36 <israelttnava6@gmail.com>
2026-01-27 10:57:35 -0700

    refactor: utilizar una sola funcion para jugador

--- src/main.cpp
+++ src/main.cpp
@@ -46,36 +46,7 @@ bool verificar_ganador(char tablero[]){
     return false;
 }
 
-void jugador1(char tablero[]){
-    int numero=0;
-    bool movimientoValido=false;
-
-    do{
-        std::cout<<"Jugador 1: X\n";
-        mostrar_tablero(tablero);
-        std::cout<<"Ingrese un numero para poner su ficha: ";
-        std::cin>>numero;
-
-        if(numero<1||numero>9){
-            std::cout<<"Numero invalido, intente de nuevo";
-            system("pause");
-            system("cls");
-            continue;
-        } else {
-            if(tablero[numero-1]=='X'||tablero[numero-1]=='O'){
-                std::cout<<"Esa casilla ya esta ocupada, intente de nuevo";
-                system("pause");
-                system("cls");
-            } else {
-                tablero[numero-1]='X';
-                system("cls");
-                movimientoValido=true;
-            }
-        }
-    } while(!movimientoValido);
-}
-
-void jugador2(char tablero[]){
+void jugador(char tablero[], char ficha){
     int numero=0;
     bool movimientoValido=false;
 
@@ -89,14 +60,13 @@ void jugador2(char tablero[]){
             std::cout<<"Numero invalido, intente de nuevo";
             system("pause");
             system("cls");
-            continue;
         } else {
             if(tablero[numero-1]=='X'||tablero[numero-1]=='O'){
                 std::cout<<"Esa casilla ya esta ocupada, intente de nuevo";
                 system("pause");
                 system("cls");
             } else {
-                tablero[numero-1]='O';
+                tablero[numero-1]=ficha;
                 system("cls");
                 movimientoValido=true;
             }
@@ -109,7 +79,7 @@ int main(){
     int i=0;
 
     while(true){
-        jugador1(tablero);
+        jugador(tablero, 'X');
         if(verificar_ganador(tablero)==true){
             break;
         }
@@ -118,7 +88,7 @@ int main(){
             std::cout<<"Es un empate\nCasillas ocupadas\n";
             break;
         }
-        jugador2(tablero);
+        jugador(tablero, 'O');
         if(verificar_ganador(tablero)==true){
             break;
         }
